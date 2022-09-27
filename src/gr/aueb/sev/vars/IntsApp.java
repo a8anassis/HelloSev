b188e1a44ebc4d6221f5690edba9ad9ffd3a7015
Th. Androutsos <a8anassis@gmail.com>
2022-09-27 18:34:02 +0300

    Initial Commit

---
+++ src/gr/aueb/sev/vars/IntsApp.java
@@ -0,0 +1,14 @@
+package gr.aueb.sev.vars;
+
+public class IntsApp {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result;
+
+        result = num1 + num2;
+
+        System.out.println("Result: " + result);
+    }
+}
